d43e7f72545facd142925084fa1609d33606d2a6
yoshioka11 <yoshioka.yuuki11@gmail.com>
2017-02-12 20:35:34 +0900

    login機能完成

--- app.js
+++ app.js
@@ -8,22 +8,88 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// var post = require('./routes/post');
+var mongoose = require('mongoose');
+var date = require('date-utils');
+var connection = mongoose.connect('mongodb://localhost/PictFight');
+//DBのlistIDでauto incrementを使いたいので定義
+var autoIncrement = require("mongoose-auto-increment");
+
+autoIncrement.initialize(connection);
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+var character = ["images/dwarf.png","images/elf.png","images/man.png","images/woman.png"];
+
+//内容スキーマ設計(チェック,内容,作成日,期限)
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  userName     : String,
+  password     : String,
+  createdDate : {type: Date, default: Date.now},
+  character  : String,
+  pussive : Number,
+  win : {type:Number ,default: 0},
+  lose : {type:Number ,default: 0}
+});
+userSchema.plugin(autoIncrement.plugin, {model:'User',field:'userId'});
+mongoose.model('User', userSchema);
+var User = mongoose.model('User');
+
+//ログインチェック登録しているIDとパスワードであれば値を返す。でなければfalse
+app.post('/loginCheck',function(req,res){
+  var userName = req.body.userName;
+  var password  = req.body.password;
+  console.log("userName:"+userName);
+  console.log("password:"+password);
+  User.find({userName:userName,password:password},function(err,check){
+    if(check.length){
+      res.send(check);
+    }else{
+      res.send(false);
+    }
+  });
+});
+
+//新しく登録する。IDが被っていないかつIdが３文字以上かつpasswordが４文字以上ならtrue
+app.post('/createUser',function(req,res){
+  var userName = req.body.userName;
+  var password = req.body.password;
+  var pussive = req.body.pussive;
+  var character = req.body.character;
+  console.log(userName,password,pussive,character);
+  User.find({userName:userName},function(err,check){
+    console.log(check);
+    if(check.length){
+      res.send(false);
+      console.log("重複");
+    }else{
+      var user = new User();
+
+      user.userName = userName;
+      user.password = password;
+      user.pussive = pussive;
+      user.character = character;
+      user.save();
+      res.send(true);
+    }
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('top', { title: 'Express' });
 });
 
 module.exports = router;
